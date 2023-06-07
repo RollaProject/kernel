c850e7d9e262b356c7bea5981351fad116d42114
0xca11 <0xca11@protonmail.com>
2023-06-07 10:54:16 +0700

    refactor: expect digest passed to be a hashed typed data already

--- src/validator/SessionKeyOwnedValidator.sol
+++ src/validator/SessionKeyOwnedValidator.sol
@@ -40,18 +40,19 @@ contract SessionKeyOwnedValidator is IKernelValidator {
         address recovered = ECDSA.recover(hash, _userOp.signature);
 
         SessionKeyStorage storage sessionKey = sessionKeyStorage[recovered][msg.sender];
-        if (sessionKey.validUntil == 0 ) { // we do not allow validUntil == 0 here
+        if (sessionKey.validUntil == 0) {
+            // we do not allow validUntil == 0 here
             return SIG_VALIDATION_FAILED;
         }
         return _packValidationData(false, sessionKey.validUntil, sessionKey.validAfter);
     }
 
     function validateSignature(bytes32 hash, bytes calldata signature) public view override returns (uint256) {
-        bytes32 ethhash = ECDSA.toEthSignedMessageHash(hash);
-        address recovered = ECDSA.recover(ethhash, signature);
+        address recovered = ECDSA.recover(hash, signature);
 
         SessionKeyStorage storage sessionKey = sessionKeyStorage[recovered][msg.sender];
-        if (sessionKey.validUntil == 0 ) { // we do not allow validUntil == 0 here
+        if (sessionKey.validUntil == 0) {
+            // we do not allow validUntil == 0 here
             return SIG_VALIDATION_FAILED;
         }
         return _packValidationData(false, sessionKey.validUntil, sessionKey.validAfter);
